4a42f53d1eb4a7259bb94bd333f14a1c902954c9
Joel <joeljesusmot@gmail.com>
2018-05-22 18:41:18 -0400

    ajuste

--- SistemaDeTurnos/SistemaDeTurnos/Controllers/HomeController.cs
+++ SistemaDeTurnos/SistemaDeTurnos/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SistemaDeTurnos.Models;
+//
+using SistemaDeTurnos.Controllers;
 
 namespace SistemaDeTurnos.Controllers
 {
