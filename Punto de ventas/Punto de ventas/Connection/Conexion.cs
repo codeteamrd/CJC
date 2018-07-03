afb2278a91bdbff0eba3185da0042041cec6f57f
Joel <joeljesusmot@gmail.com>
2018-07-03 18:21:01 -0400

    puntodeventa

---
+++ Punto de ventas/Punto de ventas/Connection/Conexion.cs
@@ -0,0 +1,15 @@
+﻿using LinqToDB.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto_de_ventas.Connection
+{
+    class Conexion : DataConnection
+    {
+        /*public Conexion() : base("conex") { }*/
+
+    }
+}
